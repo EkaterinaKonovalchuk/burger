85c4d72671d4faa4db4477693e099a2b425ad2aa
Ekaterina Konovalchuk <katy-konovalchu@yandex.ru>
2019-02-17 00:22:22 +0700

    send_form

--- main.js
+++ main.js
@@ -94,3 +94,160 @@ $(document).ready(function () {
     });
   
   });
+
+
+  
+
+
+    const left = document.querySelector("#left");
+    const right = document.querySelector("#right");
+    const items = document.querySelector("#items");
+    const minRight = 0;
+/*const maxRight = 600;
+const step = 100;
+let currentRight = 0;
+
+items.style.right = currentRight;
+
+right.addEventListener("click", function() {
+  if (currentRight < maxRight) {
+    currentRight += step;
+    items.style.right = currentRight + "px";
+  }
+});
+
+left.addEventListener("click", function() {
+  if (currentRight > minRight) {
+    currentRight -= step;
+    items.style.right = currentRight + "px";
+  }*/
+    const computed = getComputedStyle(items);
+    
+  
+    right.addEventListener("click", function(e){
+        e.preventDefault();
+
+        let currentRight= parseInt(computed.right);
+        console.log(computed.right);
+        if (!currentRight) {
+            currentRight = 0;
+          }
+          if (currentRight < 1500) {
+            items.style.right = currentRight + 100 + "px";
+          }
+        });
+        
+        left.addEventListener("click", function(e) {
+          e.preventDefault();
+
+        
+          let currentRight = parseInt(computed.right);
+        
+          if (!currentRight) {
+            currentRight = 0;
+          }
+        
+          if (currentRight > 0) {
+            items.style.right = currentRight - 100 + "px";
+          }
+    });
+
+
+
+   
+  
+
+const button = document.querySelector("#reviews__modal");
+const template = document.querySelector('#modal-template').innerHTML;
+//const button = document.querySelector(".popup");
+//const button = document.querySelector(".popup__close");
+const modal = createModal();
+
+button.addEventListener("click", e => {
+  modal.setContent("Hello WOrld");
+  modal.open();
+
+  setTimeout(() => {
+    modal.close();
+  }, 3000);
+});
+
+/*function test() {
+  const a = 10;
+  const b = a + 10;
+  ///
+
+  if (b > 20) {
+    return "yes";
+  } else {
+    return "no";
+  }
+}
+
+const result = test();
+
+console.log(result + 20);
+
+*/
+function createModal() {
+  const container = document.createElement("div");
+  container.className = "popup";
+  container.innerHTML = template;
+
+  const contentBlock = container.querySelector(".popup__content");
+
+  const closeBtn = container.querySelector(".popup__close");
+  closeBtn.addEventListener("click", e => {
+    document.body.removeChild(container);
+  });
+
+  const overlay = container.querySelector(".overlay");
+
+  overlay.addEventListener("click", e => {
+    if (e.target === overlay) {
+      closeBtn.click();
+    }
+  });
+
+  return {
+    open() {
+      document.body.appendChild(container);
+    },
+    close() {
+      closeBtn.click();
+    },
+    setContent(content) {
+      contentBlock.innerHTML = content;
+    }
+  };
+}
+
+
+//checked
+/*const no__call = document.querySelector("#no__call");
+document.getElementById('no__call').onclick = function () {
+      document.getElementById('no__call').checked = !0
+
+                }*/
+    
+
+//Form
+const myForm = document.querySelector("#myForm");
+const btn = document.querySelector("#form__btn");
+
+btn.addEventListener("click", function (e){
+e.preventDefault();
+
+const data = {
+    name: myForm.elements.name.value,
+    phone:myForm.elements.phone.value,
+    comment:myForm.elements.comment.value,
+
+}
+const xhr = new XMLHttpRequest();
+xhr.open('POST','https://webdev-api.loftschool.com/sendmail');
+xhr.send(JSON.stringify(data));
+
+//console.log(myForm.elements.name.value);
+//console.log(myForm.elements.callback.checked);
+})
